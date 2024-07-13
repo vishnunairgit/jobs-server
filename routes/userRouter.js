0c7451c2d5dbfbe2ae52fa7debbdf15995aee0ba
vishnu nair <infovishnunair@gmail.com>
2024-07-13 16:51:24 +0530

    13-07-2024

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const upload = require('../config/multerConfig');
+const { adminAuth } = require('../Middlewares/Authorization');
 
 
 
-router.get('/Getuser', userController.Getuser)
-router.put('/updateUser/:userId', upload, userController.updateUser)
-router.put('/updateUserPassword/:userId', userController.updateUserPassword)
+router.get('/Getuser', adminAuth,  userController.Getuser)
+router.put('/updateUser/:userId', upload,adminAuth, userController.updateUser)
+router.put('/updateUserPassword/:userId',adminAuth, userController.updateUserPassword)
 
 
 
